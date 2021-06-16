9c24b9c20f19d6a2f1941946cc7ba75076856bd1
mohamed-saud <mo7amed.saud2020@gmail.com>
2021-06-16 03:55:00 +0200

    add function

--- functions/superman.js
+++
@@ -1,8 +0,0 @@
-exports.handel = async () => {
-  const data = { name: "mohamd", age: 22, job: "fron-end " };
-  console.log("function is run");
-  return {
-    status: 200,
-    data: JSON.stringify(data),
-  };
-};
